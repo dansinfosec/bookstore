f2ce766ccb8a24c942b926e6e18ccf226d4b650a
dansinfosec <danberger08@gmail.com>
2023-05-09 15:23:33 +0100

    unit-tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 from django.urls import reverse
 
 
@@ -9,5 +9,19 @@ class HomePageViewTests(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
     def test_view_displays_home_page_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+
+class HomePageTests(SimpleTestCase):
+    # its best top be over descriptive with your unit tests.
+    # each method must start with test
+    # the two tests here check that the http status code of the homepage equals 200
+    # we're creating a variable called response and then uses assertEqual to check that the status code matches 200
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_url_name(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
